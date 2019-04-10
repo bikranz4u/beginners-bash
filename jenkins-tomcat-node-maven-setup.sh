bc7aa5299a09a03d50e7d04bbc4c5b1c0d3ff9b0
Bikram <bikram.keshari.das@gmail.com>
2019-04-10 17:41:01 +0530

    reference links and version updated

--- jenkins-tomcat-node-maven-setup.sh
+++ jenkins-tomcat-node-maven-setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# ------- References --------- #
+# https://www.vultr.com/docs/how-to-install-apache-tomcat-8-on-centos-7 ##
+# https://www.digitalocean.com/community/tutorials/how-to-install-apache-tomcat-8-on-centos-7
+
+
 #--------------   updating and installing requirequired packages --------------  #
 
 yum update -y
 yum install wget curl git  -y
-
 
 #--------------   Installing Java 1.8 --------------  #
 
@@ -21,28 +25,44 @@ alternatives --set javac /opt/jdk1.8.0_201/bin/javac
 
 #--------------   Setting up Java Path --------------  #
 
-echo "export JAVA_HOME=/opt/jdk1.8.0_201\nexport JRE_HOME=/opt/jdk1.8.0_201/jre\nexport PATH=$PATH:/opt/jdk1.8.0_201/bin:/opt/jdk1.8.0_191/jre/bin" > /etc/profile.d/java.sh
+cat <<EOT >> /etc/profile.d/java.sh
+#!/bin/bash
+export JAVA_HOME=/opt/jdk1.8.0_201
+export JRE_HOME=/opt/jdk1.8.0_201/jre
+export PATH=$PATH:/opt/jdk1.8.0_201/bin:/opt/jdk1.8.0_191/jre/bin
+EOT
 
 chmod +x /etc/profile.d/java.sh
 
 # Reload the java.sh , so that the path will available for all user in the system
 source /etc/profile.d/java.sh
 
+# --------------  Create a dedicated user for Apache Tomcat -------------- # 
+#For security purposes, you need to create a dedicated non-root user "tomcat" who belongs to the "tomcat" group:
+sudo groupadd tomcat
+sudo mkdir /opt/tomcat
+sudo useradd -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
+
 
 
 # --------------  Install Tomcat 8.5 --------------------------#
 cd /opt
-wget https://www-eu.apache.org/dist/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz
-tar xvf apache-tomcat-8*tar.gz -C /opt
+version=8.5.39
+wget https://www-eu.apache.org/dist/tomcat/tomcat-8/v${version}/bin/apache-tomcat-${version}.tar.gz
+tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
 
 #--------------   Setting up Tomcat Path --------------  #
-echo "#!/bin/bash\nCATALINA_HOME=/opt/tomcat-8.5.37\nPATH=$CATALINA_HOME/bin:$PATH\nexport PATH CATALINA_HOME\nexport CLASSPATH=." > /etc/profile.d/tomcat.sh
+cat <<EOT >> /etc/profile.d/tomcat.sh
+#!/bin/bash
+CATALINA_HOME=/opt/tomcat-8.5.37
+export PATH=$PATH:$CATALINA_HOME/bin
+EOT
 
 chmod +x /etc/profile.d/tomcat.sh
-source  /etc/profile.d/tomcat.sh
 chmod +x $CATALINA_HOME/bin/startup.sh
 chmod +x $CATALINA_HOME/bin/shutdown.sh
 chmod +x $CATALINA_HOME/bin/catalina.sh
+source  /etc/profile.d/tomcat.sh
 
 # -----------------------Tomocat Start / Stop short Link ----------------------- #
 
